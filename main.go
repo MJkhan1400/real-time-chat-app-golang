011991fa7e602edd2febd18e452ad66cdcbd6f38
MJkhan1400 <mjkhan1400@gmail.com>
2026-01-09 20:17:13 +0400

    feat: completed Basic WebSocket broadcast chat server in Go

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/MJkhan1400/real-time-chat-app-golang/internal/server"
+)
+
 func main() {
+	manager := &server.ClientManager{
+		Clients:    make(map[*server.Client]bool),
+		Register:   make(chan *server.Client),
+		Unregister: make(chan *server.Client),
+		Brodcast:   make(chan []byte),
+	}
+
+	go manager.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		server.ServerWS(manager, w, r)
+	})
+
+	log.Println("Server started on : 12345")
+	http.ListenAndServe(":12345", nil)
 }
