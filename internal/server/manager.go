5825da45ff864386912ba19b70dcf0a6992d13a0
MJkhan1400 <mjkhan1400@gmail.com>
2026-01-09 18:55:18 +0400

    add manager event loop for go concurrency

--- internal/server/manager.go
+++ internal/server/manager.go
@@ -6,3 +6,28 @@ type ClientManager struct {
 	Unregister chan *Client
 	Brodcast   chan []byte
 }
+
+func (manager *ClientManager) Start() {
+	for {
+		select {
+		case client := <-manager.Register:
+			manager.Clients[client] = true
+
+		case client := <-manager.Unregister:
+			if _, ok := manager.Clients[client]; ok {
+				close(client.Send)
+				delete(manager.Clients, client)
+			}
+
+		case message := <-manager.Brodcast:
+			for client := range manager.Clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(manager.Clients, client)
+				}
+			}
+		}
+	}
+}
